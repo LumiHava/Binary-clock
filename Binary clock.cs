991da338aee857f3684dc934647c678cb6b103d4
LumiHava <157164118+LumiHava@users.noreply.github.com>
2024-01-20 15:38:58 +0200

    Add files via upload

---
+++ Binary clock.cs
@@ -0,0 +1,36 @@
+﻿static void Main(string[] args)
+{
+    Console.Title = "Binary Clock";
+    Console.SetWindowSize(50, 10);
+
+    while (true)
+    {
+        Console.Clear();
+        DisplayBinaryClock();
+        System.Threading.Thread.Sleep(1000);
+    }
+}
+
+static void DisplayBinaryClock()
+{
+    DateTime currentTime = DateTime.Now;
+
+    int hour = currentTime.Hour;
+    int minute = currentTime.Minute;
+    int second = currentTime.Second;
+
+    Console.WriteLine("Hour\t\tMinute\t\tSecond");
+
+    DisplayBinaryValue(hour, 5);
+    Console.Write("\t\t");
+    DisplayBinaryValue(minute, 6);
+    Console.Write("\t\t");
+    DisplayBinaryValue(second, 6);
+    Console.WriteLine();
+}
+
+static void DisplayBinaryValue(int value, int length)
+{
+    string binary = Convert.ToString(value, 2).PadLeft(length, '0');
+    Console.Write(binary);
+}
